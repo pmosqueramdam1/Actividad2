803aa098c5b885ce307516b19d5d708985d52d1d
pmosqueramdam1 <pmosqueramdam1@gmail.com>
2022-05-18 18:55:03 +0200

    Corrección del switch

--- src/Juego.java
+++ src/Juego.java
@@ -85,15 +85,10 @@ class Jugador{
     {
         String opcion="";
         int c = (int)(Math.random()*3);
-        switch(c) {
-            case 0:
-            	opcion=("piedra");
-                break;
-            case 1:
-            	opcion=("papel");
-                break;
-            case 2:
-            	opcion=("tijeras");
+        switch (c) {
+            case 0 -> opcion = ("piedra");
+            case 1 -> opcion = ("papel");
+            case 2 -> opcion = ("tijeras");
         }
         return opcion;
     }
